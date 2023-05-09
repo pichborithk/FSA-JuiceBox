4be53938d5e8b7c40ec15f848908c04dae9a35c4
Pichborith Kong <pichborith@gmail.com>
2023-05-08 20:09:12 -0700

    create function connect and builddb

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const { Client } = require('pg');
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+// const client = new Client({
+//   user: 'postgres',
+//   password: 'postgres',
+//   database: 'juicebox-dev',
+// });
+
+async function getAllUsers() {
+  const { rows } = await client.query(`
+    SELECT id, username 
+    FROM users;
+  `);
+
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO users (username, password) VALUES ($1, $2)
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `,
+      [username, password]
+    );
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  client,
+  getAllUsers,
+  createUser,
+};
